e266d7da7d7cde5140d79fd9912c020c8dadb24a
H-Dorina <dorinahorvath079@gmail.com>
2024-03-28 12:36:34 +0100

    created Librarians migration file and model

---
+++ database/migrations/2024_03_28_104659_create_librarians_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('librarians', function (Blueprint $table) {
+            $table->id();
+            $table->string('username',32)->unique()->nullable(false)->change()->min(4);
+            $table->string('password')->unique()->nullable(false)->change()->min(16);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('librarians');
+    }
+};
