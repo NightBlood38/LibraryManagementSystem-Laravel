e9325e99bde02adc9e0c3e0aca4de98845547078
Matewalker <148473036+Matewalker@users.noreply.github.com>
2024-03-29 17:25:10 +0100

    Add files via upload

---
+++ database/factories/LibrarianFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Librarian;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Librarian>
+ */
+class LibrarianFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected static ?string $password;
+    protected $model = Librarian::class;
+     
+    public function definition(): array
+    {    
+        return [
+            'username' => $this->faker->userName,
+            'password' => static::$password ??= Hash::make('password')
+        ];
+    }
+}
