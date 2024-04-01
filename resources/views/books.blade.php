1c81b731e54e6f973825c5f8644f47d8e3f30f3c
Szabolcs <tozser.szabi@gmail.com>
2024-04-02 00:22:37 +0200

    added views

---
+++ resources/views/books.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Management System</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>Könyvtári kezelő felület</h1>
+        </div>
+    </header>
+    
+    <nav>
+        <div class="container">
+            <ul>
+                <li><a href="welcome">Kezdőoldal</a></li>
+                <li><a href="#">Könyvek</a></li>
+                <li><a href="authors">Szerzők</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    <div class="container">
+        <p>Üdvözöllek a könyvek oldalon</p>
+    </div>
+    
+    <footer>
+        <div class="container">
+            <p>&copy; 2024 Library Management System. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
